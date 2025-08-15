bac5bb8a11a3deaa97856a93f21e626b23d4c1c9
saqibshah <syedsaqibshah0011@gmail.com>
2025-08-15 16:28:40 +0500

    mobile menu

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-import { CgGym } from "react-icons/cg";
+import { CgGym, CgMenuRight } from "react-icons/cg";
 import { useSession } from "next-auth/react";
 import {
   Avatar,
@@ -15,37 +15,67 @@ import {
 } from "@radix-ui/themes";
 import { Skeleton } from "./components";
 
+const links = [
+  { label: "Dashboard", href: "/" },
+  { label: "Clients", href: "/clients" },
+  { label: "Trainers", href: "/trainers" },
+  { label: "Group Classes", href: "/group-classes" },
+  { label: "Payments", href: "/payments" },
+];
+
 const NavBar = () => {
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
-        <Flex justify="between">
+        <Flex justify="between" align="center">
           <Flex align="center" gap="3">
             <Link href="/">
               <CgGym />
             </Link>
             <NavLinks />
           </Flex>
-          <AuthStatus />
+          <Flex align="center" gap="3">
+            <AuthStatus />
+            <MobileMenu />
+          </Flex>
         </Flex>
       </Container>
     </nav>
   );
 };
 
-const NavLinks = () => {
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Clients", href: "/clients" },
-    { label: "Trainers", href: "/trainers" },
-    { label: "Group Classes", href: "/group-classes" },
-    { label: "Payments", href: "/payments" },
-  ];
-
+const MobileMenu = () => {
   const currentPath = usePathname();
-
   return (
-    <ul className="flex space-x-6">
+    <Box className="block md:!hidden">
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <CgMenuRight />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          {links.map((link) => (
+            <DropdownMenu.Item asChild key={link.href}>
+              <Link
+                href={link.href}
+                className={classNames({
+                  "!text-zinc-900": link.href === currentPath,
+                  "nav-link": true,
+                })}
+              >
+                {link.label}
+              </Link>
+            </DropdownMenu.Item>
+          ))}
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+  return (
+    <ul className="space-x-6 hidden md:flex">
       {links.map((link) => (
         <li key={link.href}>
           <Link
